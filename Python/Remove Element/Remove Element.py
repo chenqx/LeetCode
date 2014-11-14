6715746cfcf7686e1b6847b74f1d4a9598561496
chenqx <951462490@qq.com>
2014-11-14 11:27:30 +0800

    first commit

---
+++ Python/Remove Element/Remove Element.py
@@ -0,0 +1,17 @@
+#Source : https://oj.leetcode.com/problems/remove-element/
+#Problem Description
+'''
+Given an array and a value, remove all instances of that value in place and return the new length.
+
+The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+
+'''
+#Tags: Array, Two pointers
+class Solution:
+    # @param    A       a list of integers
+    # @param    elem    an integer, value need to be removed
+    # @return an integer
+    def removeElement(self, A, elem):
+        while elem in A:
+            A.remove(elem)
+        return len(A)
